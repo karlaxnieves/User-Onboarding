96323e0d7fd2272bc934141d5372e315eb20bec6
karlaxnieves <78055815+karlaxnieves@users.noreply.github.com>
2021-04-21 13:57:16 -0700

    started working on Form.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,29 +5,119 @@ import schema from './validation/schema'
 import Form from './Form'
 import './App.css';
 
-const initialFormValue = {
+const initialFormValues = {
   name: '',
   email: '',
   password: '',
-  terms: false,
+  accept: false,
 }
 
-const initialFormErrora = {
+const initialFormError = {
   name: '',
   email: '',
   password: '',
 }
 
+
+const initialUser = [];
+const initialDisabled = true;
 
 export default function App() {
   const [user, setUser] = useState({});
-  const [form, setFormValues] = useState(initialFormValue);
+  const [form, setFormValues] = useState(initialFormValues);
+  const [formError, setFormError] = useState(initialFormError);
+  const [disabled, setDisables] = useState(initialDisabled)
 
+
+  const getUsers = () => {
+    axios
+      .get('https://reqres.in/api/users')
+      .then(res => {
+        setUser(res.data)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }
+
+  const postNewUser = newUser => {
+    axios
+      .post('https://reqres.in/api/users', newUser)
+      .then(res => {
+        setUser([res.data, ...user])
+        setFormValues(initialFormValues)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }
+
+  const inputChange = (name, value) => {
+
+
+    yup
+      .reach(schema, name) // get to this part of the schema
+      //we can then run validate using the value
+      .validate(value) // validate this value
+      .then(() => {
+        // happy path and clear the error
+        setFormErrors({
+          ...formErrors,
+          [name]: "",
+        });
+      })
+      // if the validation is unsuccessful, we can set the error message to the message
+      // returned from yup (that we created in our schema)
+      .catch((err) => {
+        setFormErrors({
+          ...formErrors,
+          // validation error from schema
+          [name]: err.errors[0],
+        });
+      });
+
+    setFormValues({
+      ...formValues,
+      [name]: value,
+    });
+  };
+
+  const formSubmit = () => {
+    const newUser = {
+      name: formValues.name.trim(),
+      email: formValues.email.trim(),
+      password: formValues.password.trim(),
+      terms: ['accept'].filter(
+        (term) => formValues[term]
+      ),
+    }
+    postNewUser(newUser)
+  }
+
+  useEffect(() => {
+    getUsers();
+  }, [])
+
+  useEffect(() => {
+    schema.isValid(formValues).then((valid) => {
+      setDisabled(!valid);
+    });
+  }, [formValues]);
 
 
   return (
     <div className="App">
+      <header>
+        <h1> User App</h1>
+      </header>
 
+      <Form
+        values={formValues}
+        change={inputChange}
+        submit={formSubmit}
+        disabled={disabled}
+        errors={formErrors}
+      />
     </div>
   );
 }
---
+++ my-app/src/Form.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function Form(props) {
+    const { values, submit, change, disabled, errors } = props;
+}
